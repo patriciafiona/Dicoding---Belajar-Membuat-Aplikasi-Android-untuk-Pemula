03247cfd8dbe12b584b51278ce428721c08185dd
Patricia Fiona <patriciafiona3@gmail.com>
2023-09-15 10:07:08 +0700

    [Final Submission] Init Project

---
+++ Final Submission/MarioWorld/app/src/main/java/com/patriciafiona/marioworld/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.patriciafiona.marioworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.patriciafiona.marioworld.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
